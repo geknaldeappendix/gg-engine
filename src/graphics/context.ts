11c915bee8aa145170bac5e33d0c5d698db0bd3e
Bram Willem van der Kuip <vdkuipbw@gmail.com>
2024-02-21 23:28:00 +0100

    Fixes get webgl context to use webgl2

--- src/graphics/context.ts
+++ src/graphics/context.ts
@@ -1,4 +1,4 @@
-export function context_get_webgl(canvas: HTMLCanvasElement): WebGLRenderingContext {
+export function context_get_webgl2(canvas: HTMLCanvasElement): WebGL2RenderingContext {
     const gl = canvas.getContext("webgl2");
     if (!gl) throw new Error();
     return gl;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { canvas_create, canvas_fullscreen } from "./graphics/canvas";
-import { context_get_webgl } from "./graphics/context";
+import { context_get_webgl2 } from "./graphics/context";
 import { program_create } from "./graphics/shader";
 
 const canvas = canvas_create();
 canvas_fullscreen(canvas);
 document.body.appendChild(canvas);
-const gl = context_get_webgl(canvas);
+const gl = context_get_webgl2(canvas);
 
 gl.clearColor(0.0, 0.0, 1.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT)
