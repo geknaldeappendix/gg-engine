dd121a562f25f7da698589d437666d28affc2f41
Bram Willem van der Kuip <vdkuipbw@gmail.com>
2024-03-25 17:40:26 +0100

    Adds init

--- out/index.d.ts
+++ out/index.d.ts
@@ -1,1 +1,9 @@
+import { Input } from "./graphics";
 export * from "./graphics";
+export type Engine = {
+    canvas: HTMLCanvasElement;
+    input: Input;
+    gl: WebGL2RenderingContext;
+};
+export declare function gg_engine_create(): Engine;
+export declare function gg_engine_start(): void;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
+import { Input, canvas_create, canvas_input_create, canvas_resize, context_get_webgl2 } from "./graphics";
+
 export * from "./graphics"
+
+export type Engine = {
+    canvas: HTMLCanvasElement
+    input: Input
+    gl: WebGL2RenderingContext
+}
+
+function on_resize(canvas: HTMLCanvasElement, gl: WebGL2RenderingContext) {
+    canvas_resize(canvas, document.body.clientWidth, document.body.clientHeight);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+export function gg_engine_create(): Engine {
+    const canvas = canvas_create();
+    const input = canvas_input_create(canvas);
+    const gl = context_get_webgl2(canvas);
+    document.body.appendChild(canvas);
+
+    window.onresize = () => on_resize(canvas, gl);
+    on_resize(canvas, gl);
+
+    return {
+        canvas,
+        input,
+        gl
+    }
+}
+
+export function gg_engine_start() {
+
+}
